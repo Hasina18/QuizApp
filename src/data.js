3865c9791a2be2af15cf0ebe83ab14767090e7b1
Hasina18 <dudekulahasina58@gmail.com>
2025-01-06 20:57:34 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+export const data = [{
+    Question: 'What is Redux',
+    Option1:'Redux is State Management System',
+    Option2: 'Redux is State',
+    Option3: 'Redux is Data Type',
+    Option4: 'Redux is a part of HTML'
+},
+
+ {
+    Question: 'What is the primary use of Redux',
+    Option1:'To manage and centralize application sate',
+    Option2: 'To create CSS styles',
+    Option3: 'To handle API requests',
+    Option4: 'To manage HTML templates'
+
+ },
+ {
+   Question:'What does the useState hook in React do',
+   Option1: 'It creates a new component',
+   Option2: 'It manages side effects',
+   Option3: 'It stores and updates component state',
+   Option4: 'It stores and updates  state'
+
+
+ },
+
+ {
+    Question:'Which of the following is used to style React components',
+    Option1: 'Redux',
+    Option2: 'CSS-in-JS libraries (e.g., styled-components)',
+    Option3: 'React Router',
+    Option4: 'Node.js' 
+ },
+ 
+ {
+ Question:'What does React primarily focus on',
+
+Option1 : 'Server-side rendering',
+Option2 : 'Handling the UI components',
+Option3 : 'Database management',
+Option4 : 'Managing CSS styles'
+ },
+
+ {
+   Question: 'What is the primary purpose of the useEffect hook in React',
+
+Option1 : 'To manage state changes',
+Option2 : 'To perform side effects (e.g., data fetching, subscriptions)',
+Option3 : 'To style components',
+Option4 : 'To handle form validation'
+ },
+
+ {
+    Question: 'What is the primary function of a React components render() method?',
+    Option1 : 'To trigger API calls',
+    Option2: 'To manage component lifecycle',
+    Option3 : 'To return the JSX markup for rendering the components UI',
+    Option4: 'To handle state updates'
+     
+ },
+ {
+   Question: 'What is the primary function of a React components render() method',
+   Option1: 'To trigger API calls',
+   Option2: 'To manage component lifecycle',
+   Option3: 'To return the JSX markup for rendering the components UI',
+   Option4 : 'To handle state updates'
+
+ },
+ {
+   Question: 'What is the primary use of the useContext hook in React',
+
+Option1 : 'To access and share values from a Context API without prop drilling',
+Option2 : 'To update the state of a component',
+Option3 : 'To handle component lifecycle events',
+Option4 : 'To optimize performance of the component'
+ },
+ {
+ Question: 'What is the purpose of a key prop in React when rendering a list of elements',
+
+Option1: 'To uniquely identify each element for efficient re-rendering',
+Option2 : 'To define styles for the list items',
+Option3 : 'To bind the state of each item',
+Option4: 'To group list items into categories'
+ },
+];
